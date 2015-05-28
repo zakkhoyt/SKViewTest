b38a5bf8c929bf5d8416114ea3f7dced4ac96303
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-05-28 08:06:01 -0700

    initial push

--- SKViewTest/ViewController.m
+++ SKViewTest/ViewController.m
@@ -7,16 +7,35 @@
 //
 
 #import "ViewController.h"
+#import <SpriteKit/SpriteKit.h>
+#import "ZHScene.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) SKView *skView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.skView = [[SKView alloc]initWithFrame:CGRectMake(0, 0, 400, 400)];
+    self.skView.layer.masksToBounds = NO;
+    self.skView.allowsTransparency = YES;
+    self.skView.showsFPS = YES;
+    self.skView.showsNodeCount = YES;
+    self.skView.layer.borderColor = [UIColor blackColor].CGColor;
+    self.skView.layer.borderWidth = 1.0;
+    ZHScene *scene = [ZHScene sceneWithSize:self.skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    scene.backgroundColor = [UIColor clearColor];
+    [self.skView presentScene:scene];
+
+    
+    [self.view addSubview:self.skView];
+    self.skView.center = self.view.center;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
--- SKViewTest/ZHScene.m
+++ SKViewTest/ZHScene.m
@@ -8,6 +8,48 @@
 
 #import "ZHScene.h"
 
+@interface ZHScene ()
+@property (nonatomic, strong) SKEmitterNode *burstEmitter;
+@end
+
 @implementation ZHScene
 
+
+- (instancetype)initWithSize:(CGSize)size{
+    self = [super initWithSize:size];
+    if (self) {
+        NSString *burstPath = [[NSBundle mainBundle] pathForResource:@"rain" ofType:@"sks"];
+        
+        self.burstEmitter = [NSKeyedUnarchiver unarchiveObjectWithFile:burstPath];
+        self.burstEmitter.position = CGPointMake(self.size.width / 2.0, self.size.height / 2.0);
+
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    if(self.burstEmitter.parent){
+        return;
+    }
+    
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInNode:self];
+    self.burstEmitter.position = point;
+    self.burstEmitter.particleBirthRate = 0;
+    [self addChild:self.burstEmitter];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.burstEmitter.particleBirthRate = 10;
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            self.burstEmitter.particleBirthRate = 0;
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [self removeAllChildren];
+            });
+            
+        });
+
+    });
+    
+}
 @end
